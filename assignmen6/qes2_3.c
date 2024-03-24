297e2036150f51ca0f3efeed99455accd5920d4b
omkar patil_83929_DESD <omkarapatil221@gmail.com>
2024-03-24 22:16:06 +0530

    assignment6

--- assignmen6/qes2_3.c
+++ assignmen6/qes2_3.c
@@ -1,2 +1,28 @@
 #include<stdio.h>
 #include<string.h>
+char *u_strcat(char *,char *);
+int main()
+{
+   char ch1[20];
+   char ch2[20];
+   printf("\n enter first string");
+   scanf("%[^\n]s",ch1);
+   getch();
+   printf("enter second string");
+  	gets(ch2);
+  // scanf("%[^\n]s",ch2);
+   u_strcat(ch2,ch1);
+   printf("\n after concatenation ch2 is %s",ch2);
+	return 0;
+}
+char *u_strcat(char *dest,char *src)
+{
+	int i=0;
+	while(dest[i]!='\0')
+  		i++;
+
+	for(int j=0;src[j]!='\0';j++)
+		dest[i++]=src[j];
+	 
+	 return dest;
+}
